4a3d4966d44d359d02bbcd9fac3a062775284bf0
fmy <1791477899@qq.com>
2023-07-11 23:00:19 +0800

    写Varbar

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,38 +1,19 @@
 <template>
-  <nav>
-    <div>Bot昵称:{{bot_name}}</div>
-    <div>Bot战力:{{bot_rating}}</div>
-  </nav>
-  <router-view/>
+ <nav-bar></nav-bar>
+  <router-view></router-view>
 </template>
 
 
 <script>
-import $ from 'jquery';
 
-import {ref} from 'vue';
-
+import NavBar from '@/components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 export default{
-
-  name:"App",
-  setup:() => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-    $.ajax(
-    {
-      url:"http://localhost:3000/pk/getBotInfo/",
-      type:"get",
-      success:resp => {
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
-      }
-    });
-    return {
-      bot_name,bot_rating
+    components:{
+      NavBar
     }
-
-
-  }
+ 
 }
 </script>
 
