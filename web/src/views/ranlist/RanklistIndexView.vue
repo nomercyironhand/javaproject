a9bd33b017a9241fc9f0cd09bf16364b85cdadfa
fmy <1791477899@qq.com>
2023-07-12 22:50:07 +0800

    7.12小写一下网页跳转

---
+++ web/src/views/ranlist/RanklistIndexView.vue
@@ -0,0 +1,15 @@
+<template>
+
+        
+    
+    <div>排行榜</div>
+    
+    </template>
+    
+    <script>
+    </script>
+    
+    <style scoped>
+    
+    
+    </style>
---
+++ web/src/views/user/bot/UserbotIndexView.vue
@@ -0,0 +1,15 @@
+<template>
+
+        
+    
+    <div>我的BOT</div>
+    
+    </template>
+    
+    <script>
+    </script>
+    
+    <style scoped>
+    
+    
+    </style>
